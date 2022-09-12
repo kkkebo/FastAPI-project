993a14dc92c34c8d3c71e9f15bd23ced211102a1
Andrey.K <andrey-8991@yandex.ru>
2022-09-13 00:13:41 +0300

    creating routers for endpoints

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import Optional, List
-
 import uvicorn
+from api import users, sections, courses
 from fastapi import Query, FastAPI, Path
 from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI(
     title='Fast API studying',
@@ -15,37 +15,8 @@ app = FastAPI(
     license_info={
         'name': 'MIT',
     }
-
 )
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-
-@app.get('/users', response_model=List[User])
-async def get_users():
-    return users
-
-
-@app.post('/users')
-async def create_user(user: User):
-    users.append(user)
-    return 'Successs'
-
-
-@app.get('/user/{id}')
-async def get_user(
-        id: int = Path(..., description='The ID of the user you want to retrieve', gt=2),
-        q: str = Query(None, max_length=5)
-):
-    return {'user': users[id], 'query': q}
-
-
-if __name__ == '__main__':
-    # noinspection PyTypeChecker
-    uvicorn.run(app)
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
