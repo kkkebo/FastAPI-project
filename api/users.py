d22de2ffbbabf430e15d363fa18aea2a6ddb409a
Andrey.K <andrey-8991@yandex.ru>
2022-09-20 01:01:24 +0300

    adding async db connection; also using clean code style with black, flake8

--- api/users.py
+++ api/users.py
@@ -1,9 +1,9 @@
 import fastapi
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.db_setup import async_get_db, get_db
 from api.utils.courses import get_user_courses
-from database.db_setup import get_db
 from pydantic_schemas.course import Course
 from pydantic_schemas.user import UserCreate, User
 from api.utils.users import get_user, get_users, get_user_by_email, create_user
@@ -11,31 +11,42 @@ from api.utils.users import get_user, get_users, get_user_by_email, create_user
 router = fastapi.APIRouter()
 
 
-@router.get('/users', response_model=list[User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@router.get("/users", response_model=list[User])
+async def read_users(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
     users = get_users(db, skip=skip, limit=limit)
     return users
 
 
-@router.post('/users', response_model=User, status_code=201)
+@router.post("/users", response_model=User, status_code=201)
 async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db=db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email is already registered")
+        raise HTTPException(
+            status_code=400, detail="Email is already registered"
+        )
     return create_user(db=db, user=user)
 
 
-@router.get('/user/{user_id}', response_model=User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = get_user(db=db, user_id=user_id)
+@router.get("/user/{user_id}", response_model=User)
+async def read_user(user_id: int, db: AsyncSession = Depends(async_get_db)):
+    db_user = await get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return get_user(db=db, user_id=user_id)
+    return db_user
+
+
+# sync
+# @router.get('/user/{user_id}', response_model=User)
+# async def read_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = get_user(db=db, user_id=user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return get_user(db=db, user_id=user_id)
 
 
 @router.get("/users/{user_id}/courses", response_model=list[Course])
 async def read_user_courses(user_id: int, db: Session = Depends(get_db)):
     courses = get_user_courses(user_id=user_id, db=db)
     return courses
-
-
--- database/db_setup.py
+++ database/db_setup.py
@@ -1,17 +1,30 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:20102010@localhost/item_db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:20102010@localhost/item_db"
+)
+ASYNC_SQLALCHEMY_DATABASE_URL = (
+    "postgresql+asyncpg://postgres:20102010@localhost/item_db"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}, future=True, echo=True
 )
+
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True
 )
 
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -19,3 +32,9 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def async_get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
